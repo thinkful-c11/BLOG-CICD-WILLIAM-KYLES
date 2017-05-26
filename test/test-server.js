f770606544c51659450feb1db971c0d72c5d8768
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-05-26 14:35:23 -0700

    get and post working

--- test/test-server.js
+++ test/test-server.js
@@ -11,10 +11,41 @@ describe('blogTest', function(){
   after(function(){return closeServer();});
 
   //GET//////////////////////////////////////
-  it('should list blog entries on GET', function(){});
+  it('should list blog entries on GET', function(){
+    return chai.request(app)
+    .get('/blog-posts')
+    .then(res=>{
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      res.body.length.should.be.at.least(1);
+      res.body.forEach(el=>{
+        el.should.be.a('object');
+        el.should.include.keys('id','title','content','author','publishDate');
+      });
+    });
+  });
 
   //PUSH//////////////////////////////////////
-  it('should add new blog post on PUSH', function(){});
+  it('should add new blog post on PUSH', function(){
+    const newObj ={
+      title: 'my blog post',
+      content: 'here\'s some whining',
+      author: 'me!',
+    }
+    return chai.request(app)
+    .post('/blog-posts')
+    .send(newObj)
+    .then(res=>{
+      res.should.have.status(201);
+      return chai.request(app)
+      .get('/blog-posts');
+    }).then(res=>{
+      res.body[0].should.deep.equal(Object.assign(newObj,{id:res.body[0].id,publishDate:res.body[0].publishDate}));
+      res.body[0].should.be.a('object');
+      res.body[0].should.include.keys('id','title','content','author','publishDate');
+    });
+  });
 
   //PUT//////////////////////////////////////
   it('should update blog post on PUT', function(){
