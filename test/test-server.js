9fe609db4cc3aaec83fa0046f6955da2d7f75bbe
williamtwobit <william@twobit.cc>
2017-05-26 16:00:51 -0400

    testing Heroku

--- test/test-server.js
+++ test/test-server.js
@@ -6,3 +6,13 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 //testingTravis
+
+let answer = true;
+
+describe('basic true test', function(){
+  it('should assert that true is, in fact, true', function(){
+    answer.should.equal(true);
+  it('should assert that true is not, in fact, false', function(){
+    answer.should.not.equal(false);
+  });
+});
