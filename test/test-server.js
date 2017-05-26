57468dbc5a38392e8f8c30153e5c5cc4a1c2b71f
williamtwobit <william@twobit.cc>
2017-05-26 16:10:33 -0400

    how about another test??

--- test/test-server.js
+++ test/test-server.js
@@ -12,6 +12,7 @@ let answer = true;
 describe('basic true test', function(){
   it('should assert that true is, in fact, true', function(){
     answer.should.equal(true);
+  });
   it('should assert that true is not, in fact, false', function(){
     answer.should.not.equal(false);
   });
